5804d16a9eb73f02d3806218022ab9a78bdabd35
Iryna220 <ibiriszka951@gmail.com>
2023-12-19 21:46:35 +0100

    update

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,5 +1,31 @@
+import { CarsListStyle } from 'components/CarsList/CarsList.styled';
+import { Container, EmptyPage, Section } from './Page.styled';
+import { useSelector } from 'react-redux';
+import { selectFavoriteCars } from 'redux/selectors';
+import { Link } from 'react-router-dom';
+import { CarsItems } from 'components/CarsItems/CarsItems';
 const Favorite = () => {
-  return <>Favorite</>;
+  const favoriteCars = useSelector(selectFavoriteCars);
+
+  return (
+    <Section>
+      <Container $top="50px">
+        {favoriteCars?.length > 0 ? (
+          <CarsListStyle>
+            {favoriteCars?.map((car, index) => (
+              <CarsItems car={car} key={car.id} index={index} />
+            ))}
+          </CarsListStyle>
+        ) : (
+          <EmptyPage>
+            <p>
+              Please select your cars in the <Link to="/catalog">Catalog</Link>
+            </p>
+          </EmptyPage>
+        )}
+      </Container>
+    </Section>
+  );
 };
 
 export default Favorite;
